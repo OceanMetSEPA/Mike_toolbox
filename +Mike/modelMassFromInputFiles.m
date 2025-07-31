2a78d4e25fff23e95739c9d0900b15540cd69699
tedschlicke <59693840+tedschlicke@users.noreply.github.com>
2025-07-31 14:51:00 +0100

    Update modelMassFromInputFiles.m

--- +Mike/modelMassFromInputFiles.m
+++ +Mike/modelMassFromInputFiles.m
@@ -67,13 +67,13 @@ for sourceIndex=1:NSources
     NParticles=isource.number_of_particles_per_timestep;
     particleMass=isource.particleMass;
 
-    dfs0File=char(isource.dfs0File);
-    if ~isempty(dfs0File)
-        dfs0File=char(GetFullPath(fullfile(p,dfs0File)));
-        if ~isfile(dfs0File)
-            error('File ''%s'' not found!',dfs0File)
+    dfs0NParticles=char(isource.dfs0NParticles);
+    if ~isempty(dfs0NParticles)
+        dfs0NParticles=char(GetFullPath(fullfile(p,dfs0NParticles)));
+        if ~isfile(dfs0NParticles)
+            error('File ''%s'' not found!',dfs0NParticles)
         end
-        dfs0=Mike.readDfs0File(dfs0File);
+        dfs0=Mike.readDfs0File(dfs0NParticles);
         data=dfs0.data;
         t=data.dateTime;
         particlesPerTimeStep=data.value;
@@ -82,23 +82,25 @@ for sourceIndex=1:NSources
         t=t0+(ind0:dind:ind1)'*dtHD/spd;
         particlesPerTimeStep=repmat(NParticles,size(t));
     end
+    dfs0Mass=char(isource.dfs0Mass);
+    if ~isempty(dfs0Mass)
+        dfs0Mass=char(GetFullPath(fullfile(p,dfs0Mass)));
+        if ~isfile(dfs0Mass)
+            error('Dfs0 mass file not found (%s)',dfs0Mass);
+        end
+        dfs0=Mike.readDfs0File(dfs0Mass);
+        particleMass=dfs0.data.value;
+    end
     % Now we have number of particles per timestep, either from dfs0 file
     % or based on m21fm file settings.
     % Scale these to appropriate mass:
     if ~perParticle
         particlesPerTimeStep=particlesPerTimeStep>0;
     end
-    massPerTimeStep=particlesPerTimeStep*particleMass;
+    massPerTimeStep=particlesPerTimeStep.*particleMass;
     if asFlux
         massPerTimeStep=massPerTimeStep*dtHD;
     end
-    % CHECK
-    % check=repmat(Mike.massPerTimeStep(isource),length(t),1);
-    % if ~isequal(check,massPerTimeStep)
-    %     check
-    %     massPerTimeStep
-    %     error('MISMATCH!')
-    % end
 
     % Now we have mass of particles per timestep.
     % So we can calculate mass within model, factoring in decay.
@@ -110,15 +112,12 @@ for sourceIndex=1:NSources
     % % Calculate mass in model
     % Convolution works for non-uniform input:
     mass=conv(massPerTimeStep,timeFilter);
-    assignin('base','massPerTimeStep',massPerTimeStep)
-    assignin('base','timeFilter',timeFilter)
-    assignin('base','massFromConv',mass)
+
     % % Crop to appropriate length (Has length 2Nt-1 for equal length
     % inputs)
     % Also, conv applies input at start of timestep. Want 1st one to be
     % zero, so offset:
     mass=[0;mass(1:length(t)-1)];
-    assignin('base','mass',mass)
 
     % Theoretical mass with exponential decay (CONSTANT MASS PER TIMESTEP):
     massPerSecond=mean(massPerTimeStep)/dtHD;
